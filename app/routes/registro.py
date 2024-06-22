ab0a848a2aba13c9decd92ebed39cbb6504d5720
Mauro <mauroadauto9@gmail.com>
2024-06-22 01:17:31 -0500

    feat: añadir hasheo de contraseña al registro y actualizar creación de modelo

--- app/routes/registro.py
+++ app/routes/registro.py
@@ -1,19 +1,17 @@
 from __init__ import app, db
 from flask import jsonify, request
 from models.cliente import Cliente
-from datetime import datetime
+from werkzeug.security import generate_password_hash
 
-# REGISTRAR NUEVO CLIENTE
 @app.route('/registro', methods=['POST'])
 def create_cliente():
     data = request.get_json()
+    hashed_password = generate_password_hash(data['contrasenia'], method='pbkdf2:sha256')
     new_cliente = Cliente(
-        id_cliente=data['id_cliente'],
         nombre=data['nombre'],
         apellido=data['apellido'],
         correo_electronico=data['correo_electronico'],
-        contrasenia=data['contrasenia'],
-        fecha_registro=datetime.strptime(data['fecha_registro'], '%Y-%m-%d'),
+        contrasenia=hashed_password,
         id_tipocliente=data['id_tipocliente']
     )
     db.session.add(new_cliente)
