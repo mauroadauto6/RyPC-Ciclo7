b0a7e4328acb28d12ca0b68329222642883e4644
Mauro <mauroadauto9@gmail.com>
2024-06-22 01:15:31 -0500

    feat: añadir CORS para comunicacón API - Front

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@bdd-tikafarma/tikafarmadb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+CORS(app)
 
 from routes import *
 
