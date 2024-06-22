ae27cbdd169e08020e1190e98bde3425cf7ae680
Mauro <mauroadauto9@gmail.com>
2024-06-22 01:15:58 -0500

    feat: actualizar modelo Cliente

--- app/models/cliente.py
+++ app/models/cliente.py
@@ -3,13 +3,10 @@ from __init__ import db
 class Cliente(db.Model):
     __tablename__ = 'cliente'
     __table_args__ = {'extend_existing': True}
-    id_cliente = db.Column(db.String(255), primary_key=True)
+    id_cliente = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(255), nullable=False)
     apellido = db.Column(db.String(255), nullable=False)
     correo_electronico = db.Column(db.String(255), nullable=False, unique=True)
     contrasenia = db.Column(db.String(255), nullable=False)
-    fecha_registro = db.Column(db.Date, nullable=False)
+    fecha_registro = db.Column(db.Date, nullable=False, default=db.func.current_date())
     id_tipocliente = db.Column(db.Integer, db.ForeignKey('tipocliente.id_tipocliente'), nullable=False)
-
-    def __repr__(self):
-        return f'<Cliente {self.nombre} {self.apellido}>'
