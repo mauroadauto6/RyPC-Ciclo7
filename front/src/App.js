29930e4636956162deaa8fa2ca8dd504638d5e2f
Mauro <mauroadauto9@gmail.com>
2024-06-22 01:24:25 -0500

    feat: añadir componentes nuevos al App principal

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
 import Home from './pages/Home';
 
+import AuthModal from './components/AuthModal';
+import { AuthProvider } from './context/AuthContext';
+
 const App = () => {
-    return (
-    <Router>
-        <Navbar/>
+  const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
+
+  const handleAuthModalShow = () => setIsAuthModalOpen(true);
+  const handleAuthModalClose = () => setIsAuthModalOpen(false);
+
+  return (
+    <AuthProvider>
+      <Router>
+        <Navbar handleAuthModalShow={handleAuthModalShow} />
         <Routes>
-            <Route path='/' element={ <Home/> }>
-            </Route>
+          <Route path='/' element={<Home />} />
+          {/* Otras rutas */}
         </Routes>
-        <Footer/>
-    </Router>
-    );
-  };
-  
-  export default App;
+        <Footer />
+        <AuthModal open={isAuthModalOpen} handleClose={handleAuthModalClose} />
+      </Router>
+    </AuthProvider>
+  );
+};
+
+export default App;
+
