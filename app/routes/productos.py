82f80d117ea0841009d42b23a554af6784760828
Mauro <mauroadauto9@gmail.com>
2024-06-22 13:57:50 -0500

    feat: añadir url_imagen al fetch de la ruta Producto

--- app/routes/productos.py
+++ app/routes/productos.py
@@ -5,9 +5,15 @@ import random
 
 @app.route('/products/facial-care', methods=['GET'])
 def get_facial_care_products():
-    products = Producto.query.filter_by(id_categoria=3).all()  # id_categoria=3 corresponde a 'Cuidado Facial y Corporal'
+    products = Producto.query.filter_by(id_categoria=3).all()
     result = [
-        {"id": p.id_producto, "title": p.nombre_producto, "description": p.descripcion, "price": p.precio, "image": "/path/to/image.jpg"} 
+        {
+            "id": p.id_producto, 
+            "title": p.nombre_producto, 
+            "description": p.descripcion, 
+            "price": p.precio, 
+            "image": p.url_imagen
+        } 
         for p in products
     ]
     return jsonify(result)
@@ -15,9 +21,15 @@ def get_facial_care_products():
 @app.route('/products/featured', methods=['GET'])
 def get_featured_products():
     products = Producto.query.all()
-    featured_products = random.sample(products, 2)  # Selecciona 2 productos aleatorios
+    featured_products = random.sample(products, 4)
     result = [
-        {"id": p.id_producto, "title": p.nombre_producto, "description": p.descripcion, "price": p.precio, "image": "/path/to/image.jpg"} 
+        {
+            "id": p.id_producto, 
+            "title": p.nombre_producto, 
+            "description": p.descripcion, 
+            "price": p.precio, 
+            "image": p.url_imagen
+        } 
         for p in featured_products
     ]
     return jsonify(result)
